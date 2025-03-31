3e5224e1728505d98386f2d30e2dd9d4417bc58f
DGH12 <dghedding@gmail.com>
2025-03-31 13:58:52 -0600

    filtered response

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,7 +1,10 @@
 import requests, json
 def sentiment_analyzer(text_to_analyze):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict' 
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    f_res = json.loads(response.text)
+    score = f_res["documentSentimet"]["score"]
+    label = f_res["documentSentiment"]["label"]
+    return {"score" : {{score}}, "label" : {{label}}}
