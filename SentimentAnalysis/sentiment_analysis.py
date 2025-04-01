5d626b1cf19e2c7fdb6960a116cbb0733523b010
DGH12 <dghedding@gmail.com>
2025-04-01 10:44:33 -0600

    set up to run locally

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,10 +1,30 @@
 import requests, json
 def sentiment_analyzer(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = myobj, headers=headers)
-    f_res = json.loads(response.text)
-    score = f_res["documentSentiment"]["score"]
-    label = f_res["documentSentiment"]["label"]
-    return {"score" : {score}, "label" : {label}}
+    url = "http://"
+    headers = {"Content-Type": "application/json"}
+    prompt  =  f"""Analyze the emotion of the following text and return a JSON object with the following structure:
+    {{
+        "anger":<probability>,
+        "disgust":<probability>,
+        "fear":<probability>,
+        "joy":<probability>,
+        "sadness":<probability>,
+        "dominant_emotion": "<emotion with highest probability>"
+    }}
+    
+    Text to analyze: "{text_to_analyze}"
+    """
+    data = {
+        "model": "llama3",
+        "prompt": prompt,
+        "stream": False
+    }
+    try:
+        response = requests.post(url, json = data, headers=headers, timeout=10)
+        response.raise_for_status()
+        full_response = response.json()
+        return full_response.get("response", "")
+    except Exception as e:
+        print (f"an error occured {str(e)}")
+        
+    return response.text
--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,18 @@
 let RunSentimentAnalysis = ()=>{
     textToAnalyze = document.getElementById("textToAnalyze").value;
 
+    let data = JSON.stringify({ text: textToAnalyze });
+
     let xhttp = new XMLHttpRequest();
+
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+            let response = JSON.parse(xhttp.responseText);
+
+            document.getElementById("system_response").innerHTML = response.response || response.console.error;
         }
     };
-    xhttp.open("GET", "sentimentAnalyzer?textToAnalyze"+"="+textToAnalyze, true);
-    xhttp.send();
+    xhttp.open("POST", "/emotionDetctor", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(data);
 }
